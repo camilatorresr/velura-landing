af49f9f417e952c049d2c3f8c08152478489b29b
camilatorresr <camilaa.riosst@gmail.com>
2025-02-27 15:30:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.getElementById("orderForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    alert("¡Pedido realizado con éxito! Te contactaremos pronto.");
+    
+    let nombre = document.getElementById("nombre").value;
+    let telefono = document.getElementById("telefono").value;
+    let kit = document.getElementById("kit").value;
+
+    alert(`Gracias, ${nombre}. Hemos recibido tu pedido del ${kit}. Nos pondremos en contacto contigo pronto.`);
 });
